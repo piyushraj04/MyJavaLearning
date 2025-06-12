1969fe34fdf74d33775da2e1b2cea1185b394d0b
piyushraj04 <piyush004.raj@gmail.com>
2025-06-12 10:29:19 +0530

    msg

--- 01_Language_Fundamentals/01_Introduction/SelfPractice.java
+++ 01_Language_Fundamentals/01_Introduction/SelfPractice.java
@@ -3,5 +3,7 @@ class SelfPractice
 	public static void main(String[] args)
 	{
 		System.out.println("This Belong to my self practice on Introduction and The '+' operator:");
+		System.out.println("Practice to push code on GIT");
+		
 	}
 }
