ba770102940af85193d8522bfe05ffc05d610505
piyushraj04 <piyush004.raj@gmail.com>
2025-06-19 11:23:00 +0530

    Arithmetic Operators

---
+++ 01_Language_Fundamentals/04_Operators/Arithmetic Operators/Program2.java
@@ -0,0 +1,30 @@
+class Program2
+{
+	public static void main(String [] args)
+	{
+		int p = 7;
+		int q = 2;
+		System.out.println(p/q);
+		
+		j
+		double x = 7.0;
+		double y = 2.0;
+		System.out.println(x/y);
+		
+		
+		System.out.println(p/y);
+		System.out.println(x/q);
+		
+		
+		//System.out.println(a%b);    
+		
+	}
+}
+
+/*
+Purpose: The purpose of this program is to understand the concept of when we use two diffrent types of data then which type of throughput/result/output is comes?
+AUTHOR:PIYUSH RAJ
+Belongs to: PAWAN KR JAIN
+LOC:JSPIDER,BTM
+DATE:19-JUN-25
+*/
