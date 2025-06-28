8db806143847eb05f5f3016c7371d7522ef88074
piyushraj04 <piyush004.raj@gmail.com>
2025-06-28 11:47:08 +0530

    Square

---
+++ 01_Language_Fundamentals/05_Control_Statements/FOR/Nested For/Square5.java
@@ -0,0 +1,21 @@
+class Square5
+{
+	public static void main(String [] args)
+	{
+		int n = 5;
+	    int val = 1;
+		for(int i = 0; i < n; i++)
+		{
+			//int val = 1;
+			for(int j = 0; j < n; j++)
+			{
+				 
+				System.out.print(val+" ");
+				val++;
+			}
+			val = 1;
+			System.out.println();
+			
+		}
+	}
+}
---
+++ 01_Language_Fundamentals/05_Control_Statements/FOR/Nested For/Square8.java
@@ -0,0 +1,7 @@
+class Square8
+{
+	public static void main(String [] args)
+	{
+		
+	}
+}
