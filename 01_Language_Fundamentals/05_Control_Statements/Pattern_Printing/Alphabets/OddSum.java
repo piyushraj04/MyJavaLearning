1d3c7bf40aca5d50e82971c6980ee41b66ebb27b
piyushraj04 <piyush004.raj@gmail.com>
2025-06-30 12:07:19 +0530

    hw

---
+++ 01_Language_Fundamentals/05_Control_Statements/Pattern_Printing/Alphabets/OddSum.java
@@ -0,0 +1,18 @@
+class OddSum
+{
+	public static void main(String[] args)
+	{
+		int a = 1;
+		int b = 5;
+		int sum = 0;
+		while(a<=b)
+		{
+			if(a%2!=0)
+			{
+				sum+=a;
+			}
+			a++;
+		}
+		System.out.println(sum);
+	}
+}
---
+++ 01_Language_Fundamentals/05_Control_Statements/Pattern_Printing/Alphabets/sum.java
@@ -0,0 +1,16 @@
+class sum
+{
+	public static void main(String[] args)
+	{
+		int a = 1;
+		int b = 5;
+		int sum = 0;
+		
+		while(a<=b)
+		{
+			sum+=a;
+			a++;
+		}
+		System.out.println(sum);
+	}
+}
