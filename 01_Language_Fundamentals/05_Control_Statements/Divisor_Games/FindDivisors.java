5f7546b33c241bfd9c027a85b369fda2a435a177
piyushraj04 <piyush004.raj@gmail.com>
2025-07-01 11:15:51 +0530

    divisor_games

---
+++ 01_Language_Fundamentals/05_Control_Statements/Divisor_Games/FindDivisors.java
@@ -0,0 +1,18 @@
+class FindDivisors
+{
+	public static void main(String [] args)
+	{
+		int num = 15;
+		int a = 1;
+		while(a<=num/2)
+		{
+			if(num%a==0)
+			{
+				System.out.println(a);
+			}
+			a++;
+		}
+
+
+	}
+}
