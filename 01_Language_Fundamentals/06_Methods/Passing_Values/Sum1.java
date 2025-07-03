467f40b2b84c8fd6aa5a170814927085d6e0a8b9
piyushraj04 <piyush004.raj@gmail.com>
2025-07-03 11:05:08 +0530

    multiple args paas

---
+++ 01_Language_Fundamentals/06_Methods/Passing_Values/Sum1.java
@@ -0,0 +1,16 @@
+class Sum1
+{
+		static void test(int a,int b)
+		{
+			int sum = a+b;
+			System.out.println("Sum of "+ a +" and "+ b +" is : " + sum);
+		}
+		
+	public static void main(String [] args)
+	{
+	
+			System.out.println("Program starts");
+			test(15,10);
+			System.out.println("Program ends");
+	}
+}
