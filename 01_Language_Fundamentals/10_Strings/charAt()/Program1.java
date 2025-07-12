de21debc95ec393775eea6f088f2f5cf5d6417c7
piyushraj04 <piyush004.raj@gmail.com>
2025-07-12 23:22:29 +0530

    charAt()

---
+++ 01_Language_Fundamentals/10_Strings/charAt()/Program1.java
@@ -0,0 +1,12 @@
+class Program1
+{
+	public static void main(String[] args)
+	{
+		String str = "Developer";
+		System.out.println(str.charAt(5));
+		System.out.println(str.charAt(2));
+		System.out.println(str.charAt(8));
+		//System.out.println(str.charAt(12)); //StringIndexOutOfBoundsException
+		
+	}
+}
