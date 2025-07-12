59427cc846f0143eec1ac53674920e070aa25a23
piyushraj04 <piyush004.raj@gmail.com>
2025-07-12 23:33:31 +0530

    toCharArray()

---
+++ 01_Language_Fundamentals/10_Strings/toCharArray()/Program1.java
@@ -0,0 +1,13 @@
+class Program1
+{
+	public static void main(String[] args)
+	{
+		String str = "Developer";
+		char [] ch = str.toCharArray();
+		
+		for(int i = 0 ;  i <  ch.length ; i++)
+		{
+			System.out.println(ch[i]);
+		}
+	}
+}
