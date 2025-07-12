a4134edb19628c78ce5a8bbd53c4fd9ee874c41e
piyushraj04 <piyush004.raj@gmail.com>
2025-07-12 23:22:57 +0530

    length()

---
+++ 01_Language_Fundamentals/10_Strings/length()/Program1.java
@@ -0,0 +1,12 @@
+class Program1
+{
+	public static void main(String[] args)
+	{
+		String str = "Developer";
+		String str2 = "Java_SE24";
+		String str3 = "Java Full Stack";
+		System.out.println(str.length());
+		System.out.println(str2.length());
+		System.out.println(str3.length());		
+	}
+}
