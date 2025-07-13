8646b8ad1f8199e5dcec99a0b25ef38c199efa51
piyushraj04 <piyush004.raj@gmail.com>
2025-07-13 11:39:44 +0530

    CheckPrimeOptimized

---
+++ 01_Language_Fundamentals/08_Scanner/CheckPrimeOptimized.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class CheckPrimeOptimized
+{
+	public static boolean isPrime(int num)
+		{
+			
+			/*
+			if(num==2)     //for 2
+			{
+				return true;
+			}
+			*/
+			
+			
+			boolean status = true;
+			for(int i = 2; i <= Math.sqrt(num); i++)
+			{
+				if(num%i==0)
+				{
+					return false;
+				}
+			}
+			return true;
+
+		}
+	
+	public static void main(String [] args)
+	{
+	    Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter the number : ");
+		int a = sc.nextInt();
+	
+		System.out.println(isPrime(a));
+	}
+}
---
+++ 01_Language_Fundamentals/08_Scanner/Demo.java
@@ -0,0 +1,10 @@
+class Demo{ 
+public static void main(String[] args){ 
+add(50,20,10); 
+
+} 
+public static double add(int x, int y, int z){ 
+int sum = x+y+z; 
+return sum; 
+} 
+}
