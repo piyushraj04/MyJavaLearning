de44468873d2296a4b36522ddfcbd1a4095c0ec9
piyushraj04 <piyush004.raj@gmail.com>
2025-07-14 18:35:28 +0530

    contains

---
+++ 01_Language_Fundamentals/10_Strings/contains()/Contains.java
@@ -0,0 +1,16 @@
+class Contains
+{
+	public static void main(String[] args)
+	{
+		String str = "Engineering";
+		
+		//contains()
+		System.out.println(str.contains("job")); 
+		System.out.println(str.contains("gin"));
+		
+		//System.out.println(str.contains('p'));
+		
+	
+
+	}
+}
