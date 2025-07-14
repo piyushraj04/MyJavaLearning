86516946d1bee6a2f00d9bec79248995c96086d6
piyushraj04 <piyush004.raj@gmail.com>
2025-07-14 18:37:00 +0530

    StartsWith()

---
+++ 01_Language_Fundamentals/10_Strings/startsWith()/StartsWith.java
@@ -0,0 +1,14 @@
+class StartsWith
+{
+	public static void main(String[] args)
+	{
+		String str = "Engineering";
+	
+		//startsWith()
+		System.out.println(str.startsWith("engine"));
+		System.out.println(str.startsWith("Eng"));
+		
+		//System.out.println(str.startsWith('M'));
+
+	}
+}
