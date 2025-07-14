19de51cc311092be879d92150a89bcff676f4e55
piyushraj04 <piyush004.raj@gmail.com>
2025-07-14 11:30:55 +0530

    true

---
+++ 01_Language_Fundamentals/10_Strings/equalsIgnoreCase()/EqualsIgnoreCase.java
@@ -0,0 +1,13 @@
+class EqualsIgnoreCase
+{
+	public static void main(String[] args)
+	{
+		String str = "j2ee";
+		
+		System.out.println(str.equalsIgnoreCase("j2ee"));  //3
+		System.out.println(str.equalsIgnoreCase("J2ee"));
+		System.out.println(str.equalsIgnoreCase("adv java"));
+		
+		
+	}
+}
