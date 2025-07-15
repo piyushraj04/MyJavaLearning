b5fa8183f4e4ac30e2fbf5a3c6f70597bf2a6037
piyushraj04 <piyush004.raj@gmail.com>
2025-07-15 11:49:22 +0530

    ok

---
+++ 01_Language_Fundamentals/10_Strings/endsWith()/EndsWith.java
@@ -0,0 +1,14 @@
+class EndsWith
+{
+	public static void main(String[] args)
+	{
+		String str = "Engineering";
+	
+		//endswith()
+		System.out.println(str.endsWith("r ing"));
+		System.out.println(str.endsWith("ing"));
+		
+		System.out.println(str.endsWith('M'));
+
+	}
+}
