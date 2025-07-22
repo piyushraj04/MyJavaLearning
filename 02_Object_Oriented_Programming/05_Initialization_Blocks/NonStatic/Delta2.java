f7ea5537effccbdfe18a86ee5dd07459d805d5bc
piyushraj04 <piyush004.raj@gmail.com>
2025-07-22 11:22:51 +0530

    multi NSIB

---
+++ 02_Object_Oriented_Programming/05_Initialization_Blocks/NonStatic/Delta2.java
@@ -0,0 +1,19 @@
+class Delta2
+{
+	int a ;
+	double b;
+	char c;
+	{
+		a = 100;
+		b = 4.5;
+		c = 'J';
+	}
+	
+	public static void main(String [] args)
+	{
+		Delta2 obj = new Delta2();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println(obj.c);
+	}
+}
