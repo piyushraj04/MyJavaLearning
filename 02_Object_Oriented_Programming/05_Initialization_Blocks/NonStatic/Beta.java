61b5687578c68a7c925aba986b7d9031ee0d81bc
piyushraj04 <piyush004.raj@gmail.com>
2025-07-22 11:30:12 +0530

    multiplw NSIB

---
+++ 02_Object_Oriented_Programming/05_Initialization_Blocks/NonStatic/Beta.java
@@ -0,0 +1,23 @@
+class Beta
+{
+	int a ;
+	double b;
+	char c;
+	{
+		a = 100;
+	}
+	{
+		b = 12.56;
+	}
+	{
+		c = 'Q';
+	}
+	
+	public static void main(String [] args)
+	{
+		Beta obj = new Beta();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println(obj.c);
+	}
+}
